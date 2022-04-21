668dc9588f0d7759bca2ae43b2489aff400d6725
Yoan Cucchi <yoancucchi@MBP-de-Yoan.home>
2022-04-21 16:10:31 +0300

    more

--- srcs/ft_initialize_parameter.c
+++ srcs/ft_initialize_parameter.c
@@ -16,15 +16,15 @@
 void	parameter_print(t_parameter p)
 {
 	printf("---------------------------------------------------------------\n");
+	printf("sharp = %d\n", p.sharp);
+	printf("zero = %d\n", p.zero);
 	printf("minus = %d\n", p.minus);
 	printf("plus = %d\n", p.plus);
+	printf("space = %d\n", p.space);
 	printf("width = %d\n", p.width);
+	printf("dot = %d\n", p.dot);
 	printf("precision = %d\n", p.precision);
 	printf("specifier = %c\n", p.specifier);
-	printf("zero = %d\n", p.zero);
-	printf("dot = %d\n", p.dot);
-	printf("space = %d\n", p.space);
-	printf("sharp = %d\n", p.sharp);
 	printf("---------------------------------------------------------------\n");
 }
 
@@ -32,14 +32,14 @@ t_parameter	ft_all_to_0(void)
 {
 	t_parameter	newparameter;
 
+	newparameter.sharp = 0;
+	newparameter.zero = 0;
 	newparameter.minus = 0;
 	newparameter.plus = 0;
+	newparameter.space = 0;
 	newparameter.width = 0;
+	newparameter.dot = 0;
 	newparameter.precision = 0;
 	newparameter.specifier = 0;
-	newparameter.zero = 0;
-	newparameter.dot = 0;
-	newparameter.space = 0;
-	newparameter.sharp = 0;
 	return (newparameter);
 }
