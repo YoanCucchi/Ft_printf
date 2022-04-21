bee8fdc6e230da3e16c32f1fe5bd2d41dbe6058c
Yoan Cucchi <yoancucchi@MBP-de-Yoan.home>
2022-04-21 17:17:50 +0300

    start

--- srcs/ft_parse.c
+++ srcs/ft_parse.c
@@ -40,7 +40,7 @@ static t_parameter	ft_parse_width(char *str, va_list args, t_parameter p)
 	{
 		if (*str == '0' && !ft_isdigit(*(str - 1)))
 			p.zero = 1;
-		else if (((*str > '0' && *str <= '9') || *str == '*') && !check)
+		else if ((ft_strchr(SPECIFIERS, *str) || *str == '*') && !check)
 		{
 			if (*str == '*')
 				p.width = va_arg(args, int);
