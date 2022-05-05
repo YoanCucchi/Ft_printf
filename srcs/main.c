7e9786220699f21745568ba904864bb2521aae42
Yoan Cucchi <yoancucchi@MBP-de-Yoan.home>
2022-05-05 11:25:56 +0300

    fixed 2 leaks from libft

--- srcs/main.c
+++ srcs/main.c
@@ -393,8 +393,8 @@ int main(void)
 	// ret2 = ft_printf("mine:%s\n", "hello there");
 	// printf("%d %d\n", ret1, ret2);
 
-	ret1 = printf("norm:%u\n", 429495);
-	ret2 = ft_printf("mine:%u\n", 429495);
+	ret1 = printf("norm:%lu\n", 429495);
+	ret2 = ft_printf("mine:%lu\n", 429495);
 	printf("%d %d\n", ret1, ret2);
 	system("leaks ft_printf");
 	return (0);
